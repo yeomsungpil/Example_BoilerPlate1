31e72c9a703b854e482b3e401388cc28c89a0ef3
염성필 <ysp7256@limefriends.com>
2024-06-04 10:54:33 +0900

    add Alamofire

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "Example_BoilerPlate1",
             targets: ["Example_BoilerPlate1"]),
     ],
+    dependencies: [
+        // Alamofire 패키지를 종속성으로 추가합니다.
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Example_BoilerPlate1"),
+            name: "Example_BoilerPlate1",
+            dependencies: ["Alamofire"]),
         .testTarget(
             name: "Example_BoilerPlate1Tests",
             dependencies: ["Example_BoilerPlate1"]),
