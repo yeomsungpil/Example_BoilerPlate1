0d02d1fb38d796a47d97a9e87d987a8cc9d30bda
염성필 <ysp7256@limefriends.com>
2024-06-04 10:36:53 +0900

    add baseButton version 1.0.2

---
+++ Sources/Example_BoilerPlate1/BaseButton.swift
@@ -0,0 +1,100 @@
+//
+//  File.swift
+//  
+//
+//  Created by Limefriends on 6/4/24.
+//
+import UIKit
+
+class BaseButton: UIButton {
+    /// UIButton - 버튼 텍스트, 버튼 색, 버튼 배경색, 버튼 폰트, 버튼 폰트 사이즈, CornerRadius
+    /// - Parameters:
+    ///   - text: 버튼 텍스트
+    ///   - textColor:  버튼 텍스트 컬러
+    ///   - bgColor: 버튼 배경색
+    ///   - font: 버튼 font
+    ///   - fontSize: 버튼 fontSize
+    ///   - radius: 버튼 cornerRadius
+    ///   - borderColor: 버튼 borderColor
+    ///   - borderWidth: 버튼 borderWidth
+    ///   - minimumScaleFactor: 버튼 내부의 텍스트 사이즈 조절
+    init(text: String,
+         textColor: UIColor = .blue,
+         bgColor: UIColor = .clear,
+         font: UIFont.Weight,
+         fontSize: CGFloat,
+         radius: CGFloat?,
+         borderColor: UIColor?,
+         borderWidth: CGFloat?,
+         image: String?,
+         imageEdge: UIEdgeInsets?,
+         titleEdge: UIEdgeInsets?,
+         minimumScaleFactor: CGFloat = 1.0) {
+        super.init(frame: .zero)
+        
+
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(textColor, for: .normal)
+        self.backgroundColor = bgColor
+        self.titleLabel?.font = UIFont.systemFont(ofSize: fontSize, weight: font)
+        self.titleLabel?.adjustsFontSizeToFitWidth = true
+        self.titleLabel?.minimumScaleFactor = minimumScaleFactor // 최소 50%까지 축소 가능
+        
+        if let radius {
+            self.layer.cornerRadius = radius
+            if #available(iOS 13.0, *) {
+                self.layer.cornerCurve = .continuous
+            } else {
+                // Fallback on earlier versions
+            }
+            self.clipsToBounds = true
+        }
+        
+        if let borderColor, let borderWidth {
+            self.layer.borderColor = borderColor.cgColor
+            self.layer.borderWidth = borderWidth
+        }
+        
+        
+        
+        if let image, let imageEdge {
+            self.setImage(UIImage(named: image), for: .normal)
+            self.imageEdgeInsets = imageEdge
+        }
+        
+        if let titleEdge {
+            self.titleEdgeInsets = titleEdge
+        }
+    }
+    
+    
+    /// Tag가 필요한 Button
+    /// - Parameters:
+    ///   - title: 타이틀
+    ///   - titleColor: 타이틀 색상
+    ///   - tag: 태그
+    ///   - font: 폰트
+    ///   - fontsize: 폰트 사이즈
+    init(title: String, titleColor: UIColor, tag: Int, font: UIFont.Weight, fontsize: CGFloat) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.tag = tag
+        self.titleLabel?.font = UIFont.systemFont(ofSize: fontsize, weight: font)
+    }
+    
+    /// SNS Login Button 셋팅
+    /// - Parameter snsLogin: SNS name
+    init(snsLogin: String) {
+        super.init(frame: .zero)
+        self.setTitle("", for: .normal)
+        self.setImage(UIImage(named: snsLogin), for: .normal)
+        self.frame = CGRect(x: 0, y: 0, width: 54, height: 54)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
